8f3c49873250cb2fb17f6163673886eb1a57c3bb
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-07-19 01:24:20 -0600

    added a real basic error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/error.module.css";
+
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.errorContainer}>
+      <h1 className={styles.errorTitle}>Oops! This page doesn't exist. :(</h1>
+      <p className={styles.errorMessage}>
+        But hey, every wrong turn leads to the right vinyl eventually.
+      </p>
+      <button className={styles.errorButton} onClick={() => navigate("/")}>
+        Take me home...
+      </button>
+    </div>
+  );
 }
 
 export default ErrorPage;
